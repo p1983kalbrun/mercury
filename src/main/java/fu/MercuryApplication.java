da77dc7fd2e7664056f3304e98302967f51d3340
p1983kalbrun <53264036+p1983kalbrun@users.noreply.github.com>
2019-09-23 11:15:44 +0100

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/fu/MercuryApplication.java
@@ -0,0 +1,12 @@
+package fu;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercuryApplication {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(MercuryApplication.class, args);
+	}
+
+}
---
+++ src/main/java/fu/MercuryController.java
@@ -0,0 +1,20 @@
+package fu;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class MercuryController {
+
+	@GetMapping("/")
+	public String root() {
+		return "Hello Kubernetes demo on Wed! I'm served by " + getClass().getName();
+	}
+
+	@GetMapping("hello/{name}")
+	public String person(@PathVariable String name) {
+		return "Hello " + name + "!";
+	}
+
+}
